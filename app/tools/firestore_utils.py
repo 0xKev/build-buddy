667302f7835b783625d621f884bca046796a00c3
Amiya Lamisha <82974253+amiyalamisha@users.noreply.github.com>
2026-03-14 04:27:22 -0400

    firestore imports, skeleton
    need virtual env for firestore install properly

---
+++ app/tools/firestore_utils.py
@@ -0,0 +1,50 @@
+from typing import Literal, Optional
+from google.adk.tools.base_tool import BaseTool
+from google.adk.tools.tool_context import ToolContext
+from mcp import Tool
+from typing import Any
+from datetime import datetime, timezone
+"""
+# need virtual python environment?
+import firebase_admin
+from firebase_admin import credentials
+from firebase_admin import firestore
+"""
+
+
+
+def _build_snapshot(
+    tool: BaseTool,
+    args: dict[str, Any],
+    tool_context: ToolContext,
+    tool_response: dict,
+):
+    return {
+        "tool_name": tool.name,
+        "timestamp": datetime.now(timezone.utc),
+        "args": args,
+        "response": tool_response,
+        "build_progress": tool_context.state.get("build_progress", {}),
+        "gcs_url": "PENDING",
+    }
+
+def _update_firestore_record(doc_id: str, fields: dict):
+    # the main thing we'll only be updating is just "gcs_url"
+    upload_url = fields.get("gcs_url")
+    if upload_url:
+        # update the firestore record
+        """
+        #url to exisiting database
+        cred = credentials.Certificate('path/to/serviceAccount.json')
+
+        app = firebase_admin.initialize_app(cred)
+
+        db = firestore.client()
+        """
+        ...
+
+
+def _log_to_firestore(snapshot: dict[str, str]) -> str:
+    ...
+    # store to firestore
+    # return firestore id for this record
