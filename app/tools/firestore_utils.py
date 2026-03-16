100d2119eae3852cdf00e3db07aeff59ca9cf164
0xKev <56137695+0xKev@users.noreply.github.com>
2026-03-16 13:52:57 -0400

    feat: firestore report upload only on deltas rather than full history

--- app/tools/firestore_utils.py
+++ app/tools/firestore_utils.py
@@ -1,55 +1,122 @@
-import json
 from typing import Literal, Optional
+from annotated_types import doc
 from google.adk.tools.base_tool import BaseTool
 from google.adk.tools.tool_context import ToolContext
-from mcp import Tool
 from typing import Any
 from datetime import datetime, timezone
-import firebase_admin
-from firebase_admin import credentials
-from firebase_admin import firestore     
+from google.cloud import firestore
+
+FIRESTORE_COLLECTION_ID = "bb-report-data"
+PROJECT_ID = "build-buddy-488918"
+
+_db = firestore.Client(PROJECT_ID)
 
 
-cred = credentials.Certificate("service_account_key.json")
-firebase_admin.initialize_app(cred)
-
-# this was me testing, then add to a function after
-db = firestore.client()
-data = {'name': "Kim", 'parts': 40}
-db.collection('queries').add(data)
-
-
+# capture just the delta instead of full history
 def _build_snapshot(
     tool: BaseTool,
-    args: dict[str, Any],
     tool_context: ToolContext,
-    tool_response: dict,
 ):
+    build = tool_context.state.get("build_progress", {})
+    last_updated = tool_context.state.get("last_updated_part")
+
+    # removing args and tool_resonse from params because build_progess has sufficient info for reports
     return {
         "tool_name": tool.name,
         "timestamp": datetime.now(timezone.utc),
-        "args": args,
-        "response": tool_response,
-        "build_progress": tool_context.state.get("build_progress", {}),
+        "part_id": last_updated,
+        "part_data": build.get(last_updated, {}),
         "gcs_url": "PENDING",
     }
 
-def _update_firestore_record(doc_id: str, fields: dict):
+
+def _update_firestore_record(doc_id: str, gcs_url: str):
     # the main thing we'll only be updating is just "gcs_url"
-    """
-    db = firestore.client()
-    # adding documents with auto id
-    data = {_build_snapshot.__get__("tool_name"),_build_snapshot.__get__("timestamp"), _build_snapshot.__get__("response")}
-    db.collection('queries').add(data)
-    """
-    
-    upload_url = fields.get("gcs_url")
-    if upload_url:
-        # update the firestore record
-        ...
+    doc_ref = _db.collection(FIRESTORE_COLLECTION_ID).document(doc_id)
+    doc_ref.update({"gcs_url": gcs_url})
 
 
 def _log_to_firestore(snapshot: dict[str, str]) -> str:
-    ...
     # store to firestore
     # return firestore id for this record
+    _, doc_ref = _db.collection(FIRESTORE_COLLECTION_ID).add(snapshot)
+    return doc_ref.id  # return the doc id, not the reference
+
+
+if __name__ == "__main__":
+
+    class FakeTool:
+        def __init__(self):
+            self.name = "fake_tool_call"
+
+    class FakeToolContext:
+        def __init__(self):
+            self.state = {
+                "build_progress": {
+                    "cpu": {
+                        "name": "AMD Ryzen 5 5600X 3.7 GHz 6-Core Processor",
+                        "category": "CPU",
+                        "status": "NOT_STARTED",
+                        "notes": "",
+                    },
+                    "cooler": {
+                        "name": "Noctua NH-U12S 55 CFM CPU Cooler",
+                        "category": "CPU Cooler",
+                        "status": "NOT_STARTED",
+                        "notes": "",
+                    },
+                    "motherboard": {
+                        "name": "Asus TUF GAMING X570-PLUS (WI-FI) ATX AM4 Motherboard",
+                        "category": "Motherboard",
+                        "status": "NOT_STARTED",
+                        "notes": "",
+                    },
+                    "ram": {
+                        "name": "G.Skill Ripjaws V 32 GB (2 x 16 GB) DDR4-3200 CL16 Memory",
+                        "category": "Memory",
+                        "status": "NOT_STARTED",
+                        "notes": "",
+                    },
+                    "storage": {
+                        "name": "Crucial T500 1 TB M.2-2280 PCIe 4.0 X4 NVME Solid State Drive",
+                        "category": "Storage",
+                        "status": "NOT_STARTED",
+                        "notes": "",
+                    },
+                    "gpu": {
+                        "name": "Gigabyte AORUS ELITE GeForce RTX 3060 Ti 8 GB Video Card",
+                        "category": "Video Card",
+                        "status": "NOT_STARTED",
+                        "notes": "",
+                    },
+                    "case": {
+                        "name": "Corsair 4000D Airflow ATX Mid Tower Case",
+                        "category": "Case",
+                        "status": "NOT_STARTED",
+                        "notes": "",
+                    },
+                    "psu": {
+                        "name": "EVGA SuperNOVA 750 GA 750 W 80+ Gold Certified Fully Modular ATX Power Supply",
+                        "category": "Power Supply",
+                        "status": "NOT_STARTED",
+                        "notes": "",
+                    },
+                    "monitor": {
+                        "name": 'LG 24GL600F-B 23.6" 1920 x 1080 144 Hz Monitor',
+                        "category": "Monitor",
+                        "status": "NOT_STARTED",
+                        "notes": "",
+                    },
+                }
+            }
+
+    mock_tool = FakeTool()
+    mock_tool_context = FakeToolContext()
+    mock_gcs_url = "testing-gcs-url.com/abc"
+
+    mock_tool_context.state["last_updated_part"] = "cpu"
+
+    snapshot = _build_snapshot(mock_tool, mock_tool_context)
+    doc_id = _log_to_firestore(snapshot)
+    print(f"{doc_id=}")
+    _update_firestore_record(doc_id, mock_gcs_url)
